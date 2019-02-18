13708a29bb60926b98e154bdb91f7fb3d6d19a76
imdeepmind <infinityatme@gmail.com>
2019-02-18 23:10:00 +0530

    added age prediction system

--- detect.py
+++ detect.py
@@ -7,23 +7,33 @@ haar = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 model = keras.models.load_model('gender/gender.h5')
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
+age_model = keras.models.load_model('age/age.h5')
+age_model.compile(loss='mean_squared_error', optimizer='adam')
+
 while True:
     try:
         ret, frame = camera.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
         face = haar.detectMultiScale(gray, 1.2, 5)
+        
         for x,y,w,h in face:
             cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
             roi = gray[x:x+w, y:y+h]
+            
             target = cv2.resize(roi, (64,64))
             target = target.reshape(-1, 64,64,1)
+            
             dt = model.predict_classes(target)
+            
             if dt == '0':
                 gname = 'Female'
             else:
                 gname = 'Male'
+        
+            age = age_model.predict(target)
+            print(age[0][0]*100)
             cv2.putText(frame, gname, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
-            print(model.predict(target))
         cv2.imshow('ME', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
